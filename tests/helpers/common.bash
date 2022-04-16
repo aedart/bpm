0311275f9ae662ce992fbfd4280f580f89de7024
Alin Eugen Deac <aedart@gmail.com>
2022-04-16 16:19:36 +0200

    Add common setup helper

---
+++ tests/helpers/common.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##
+# Common setup for bats support and assert modules
+#
+# Globals:
+#   - BATS_ROOT (defined by bats)
+#
+common_setup() {
+
+    # echo "BATS ROOT >>> $BATS_ROOT"
+
+    # TODO: At some point... change these load methods...
+    load "${BATS_ROOT}/../bats-support/load"
+    load "${BATS_ROOT}/../bats-assert/load"
+
+    # Define project root
+    PROJECT_ROOT="$( cd "$( dirname "${BATS_ROOT}/../../../")" && pwd )"
+
+    # make executables in src/ visible to PATH
+    PATH="$PROJECT_ROOT/src:$PATH"
+}
