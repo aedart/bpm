a0e8c40c7879af6543678ad7de5a227a6c0c8791
Alin Eugen Deac <aedart@gmail.com>
2022-05-05 20:25:35 +0200

    Add compare method (incomplete)
    Thus far, the numeric comparison for major, minor and patch is in order.
    Also started work on pre-release identifiers comparison. This still needs work and lots of testing.

--- src/semver/semver.sh
+++ src/semver/semver.sh
@@ -130,6 +130,114 @@ semver::parse() {
     return 1
 }
 
+# TODO:
+# TODO: -1: a less than b (a < b)
+# TODO: 0: a equals b (a = b)
+# TODO: 1: a greater than b (a > b)
+semver::compare() {
+    # Arrays to hold the parsed versions
+    local -A version_a=()
+    local -A version_b=()
+
+    # Output symbols (for the sake of readability)
+    local -r A_LESS_THAN_B='-1'
+    local -r A_EQUALS_B='0'
+    local -r A_GREATER_THAN_B='1'
+
+    # TODO: Fail if num. arguments is not exactly two!
+
+    # Parse and populate local version arrays
+    semver::parse "$1" "version_a"
+    semver::parse "$2" "version_b"
+
+    # ------------------------------------------------------------------------
+
+    # Early out - if both versions are the same (full version strings comparison)
+    # Here, eventual build meta-data is also part of the comparison, which is NOT
+    # in accordance to Semantic Versioning 2.0.0 standard. Yet, its okay here...
+    if [[ "${version_a['version']}" == "${version_b['version']}" ]]; then
+        echo $A_EQUALS_B
+        exit 0
+    fi
+
+    # ------------------------------------------------------------------------
+
+    # Create two new arrays that contain only the numeric version identifiers
+    local a=("${version_a['major']}" "${version_a['minor']}" "${version_a['patch']}")
+    local b=("${version_b['major']}" "${version_b['minor']}" "${version_b['patch']}")
+
+    # Loop through the version numbers, comparing from left to right (major, minor and patch)
+    # @see https://semver.org/#spec-item-11
+    for (( i = 0; i < 3; i++ )); do
+        local vA="${a[$i]}"
+        local vB="${b[$i]}"
+
+        # Debug
+        # echo "$vA vs. $vB"
+
+        # When a is less than b
+        if [[ $vA < $vB ]]; then
+            echo $A_LESS_THAN_B
+            exit 0
+        fi
+
+        # When a is greater than b
+        if [[ $vA > $vB ]]; then
+            echo $A_GREATER_THAN_B
+            exit 0
+        fi
+
+        # Continue, to next version identifier when both are equal...
+    done
+
+    # ------------------------------------------------------------------------
+
+    # If we reached this point, it means that major, minor and patch are equal.
+    # Therefore, we must compare eventual pre-release identifiers if available.
+    # @see https://semver.org/#spec-item-11
+    local pre_release_a="${version_a['pre_release']}"
+    local pre_release_b="${version_b['pre_release']}"
+
+    # Early out - if neither a not b have pre-release identifiers...
+    if [[ -z "${pre_release_a}" && -z "${pre_release_b}" ]]; then
+        echo $A_EQUALS_B
+        exit 0
+    fi
+
+    # "[...] When major, minor, and patch are equal, a pre-release version has
+    # lower precedence than a normal version:
+    #
+    # Example: 1.0.0-alpha < 1.0.0. [...]" (#spec-item-11.3)
+
+    # When a has pre-release, but b does not... E.g. (a) 1.0.0-alpha < (b) 1.0.0
+    if [[ -n "${pre_release_a}" && -z "${pre_release_b}"  ]]; then
+        echo $A_LESS_THAN_B
+        exit 0
+    fi
+
+    # When a has no pre-release, but b does... E.g. (a) 1.0.0 > (b) 1.0.0-alpha
+    if [[ -z "${pre_release_a}" && -n "${pre_release_b}"  ]]; then
+        echo $A_GREATER_THAN_B
+        exit 0
+    fi
+
+    # ------------------------------------------------------------------------
+
+    # "[..] Precedence for two pre-release versions [...] MUST be determined
+    # by comparing each dot separated identifier from left to right until a
+    # difference is found [...]" (#spec-item-11.4)
+
+    # TODO: Split pre-releases by dot, into two arrays... loop identifiers
+    # TODO: and compare acc. to spec-item-11.4...
+
+    # ------------------------------------------------------------------------
+
+    # Finally, both version and pre-release identifiers are the same. Output
+    # and exit accordingly.
+    echo $A_EQUALS_B
+    exit 0
+}
+
 # ------------------------------------------------------------------------
 # Internals
 # ------------------------------------------------------------------------
